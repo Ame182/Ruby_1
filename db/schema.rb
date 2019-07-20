2150e62def83e43e3b78a6b1c77834303eb7ec41
amelia <amelita182@gmail.com>
2019-07-20 09:18:28 -0400

    feker gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_010041) do
+ActiveRecord::Schema.define(version: 2019_07_13_013236) do
 
   create_table "pins", force: :cascade do |t|
     t.string "photo"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
